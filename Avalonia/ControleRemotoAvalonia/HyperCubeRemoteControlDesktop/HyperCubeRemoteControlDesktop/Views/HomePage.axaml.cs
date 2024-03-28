a3c05b4892ce424d79e8546b86cd343eb91dbcb0
LucasMacedoBernardino <lucasmacedoes@gmail.com>
2024-03-28 17:07:45 -0300

    adicionando xml Mapper ao projeto

--- Avalonia/ControleRemotoAvalonia/HyperCubeRemoteControlDesktop/HyperCubeRemoteControlDesktop/Views/HomePage.axaml.cs
+++ Avalonia/ControleRemotoAvalonia/HyperCubeRemoteControlDesktop/HyperCubeRemoteControlDesktop/Views/HomePage.axaml.cs
@@ -1,27 +1,26 @@
 using Avalonia.Controls;
 using IpPortEventArgs;
 using System;
-
 namespace HyperCubeRemoteControlDesktop;
 
 public partial class HomePage : UserControl
 {
-    public HomePage()
+  public HomePage()
+  {
+    InitializeComponent();
+  }
+  public event EventHandler<NavigationEventArgs> TrocarDeTelaEpassarAsInformaçõesParaAproximaTela;
+  private void TrocarDeTela(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+  {
+    if (!(string.IsNullOrEmpty(ip.Text) || string.IsNullOrEmpty(port.Text)))
     {
-        InitializeComponent();
-    }
-    public event EventHandler<NavigationEventArgs> TrocarDeTelaEpassarAsInformaçõesParaAproximaTela;
-    private void TrocarDeTela(object sender, Avalonia.Interactivity.RoutedEventArgs e)
-    {
-        if(!(string.IsNullOrEmpty(ip.Text) || string.IsNullOrEmpty(port.Text))) 
-        {
-            var navigarionArgs = new NavigationEventArgs(ip.Text, port.Text);
-            TrocarDeTelaEpassarAsInformaçõesParaAproximaTela?.Invoke(this, navigarionArgs);
+      var navigarionArgs = new NavigationEventArgs(ip.Text, port.Text);
+      TrocarDeTelaEpassarAsInformaçõesParaAproximaTela?.Invoke(this, navigarionArgs);
 
-        }
-        else
-        {
-            mensagem.Text = "Preencha Ip e Porta!";
-        }
     }
+    else
+    {
+      mensagem.Text = "Preencha Ip e Porta!";
+    }
+  }
 }
